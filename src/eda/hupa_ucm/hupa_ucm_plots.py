956c75f685d203afc19f1bfc147b6f8b9b610e6c
rana-helal <res.maika@gmail.com>
2025-09-03 01:09:59 +0300

    Refactor plotting functions to improve outlier and distribution visualizations

--- src/eda/hupa_ucm/hupa_ucm_plots.py
+++ src/eda/hupa_ucm/hupa_ucm_plots.py
@@ -1,16 +1,22 @@
 import os
 import matplotlib.pyplot as plt
 
-
-def plot_outliers(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabetes Dataset/EDA_Outputs/outliers"):
+#  get numeric columns & create output directory
+def prepare_plotting(df, file_name, output_folder):
     numeric_cols = df.select_dtypes(include=["float64", "int64"]).columns
     if len(numeric_cols) == 0:
         print("No numeric columns to plot.")
-        return
-
-    file_base = os.path.splitext(file_name)[0]
+        return None, None
+    file_base = os.path.splitext(file_name)[0] # get file name without extension
     save_dir = os.path.join(output_folder, file_base)
     os.makedirs(save_dir, exist_ok=True)
+    return numeric_cols, save_dir
+
+# Create and save boxplots for numeric df columns to visualize outliers
+def plot_outliers(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabetes Dataset/EDA_Outputs/outliers"):
+    numeric_cols,save_dir = prepare_plotting(df, file_name, output_folder)
+    if numeric_cols is None:
+        return
 
     for col in numeric_cols:
         plt.figure(figsize=(6, 4))
@@ -22,16 +28,12 @@ def plot_outliers(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabe
 
     print(f"Saved outlier plots for {file_name} in {save_dir}")
 
+# Create and save histogram for numeric df columns to visualize outliers
 
 def plot_distributions(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabetes Dataset/EDA_Outputs/distributions"):
-    numeric_cols = df.select_dtypes(include=["float64", "int64"]).columns
-    if len(numeric_cols) == 0:
-        print("No numeric columns to plot.")
+    numeric_cols, save_dir = prepare_plotting(df, file_name, output_folder)
+    if numeric_cols is None:
         return
-
-    file_base = os.path.splitext(file_name)[0]
-    save_dir = os.path.join(output_folder, file_base)
-    os.makedirs(save_dir, exist_ok=True)
 
     for col in numeric_cols:
         plt.figure(figsize=(6, 4))
