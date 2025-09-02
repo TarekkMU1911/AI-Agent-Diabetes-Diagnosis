80c9cf5d045d016efb6170a02827165260d6b2b1
rana-helal <res.maika@gmail.com>
2025-09-03 02:27:09 +0300

    Refactor plotting functions

--- src/eda/hupa_ucm/hupa_ucm_plots.py
+++ src/eda/hupa_ucm/hupa_ucm_plots.py
@@ -1,22 +1,17 @@
 import os
 import matplotlib.pyplot as plt
 
-#  get numeric columns & create output directory
-def prepare_plotting(df, file_name, output_folder):
-    numeric_cols = df.select_dtypes(include=["float64", "int64"]).columns
-    if len(numeric_cols) == 0:
-        print("No numeric columns to plot.")
-        return None, None
-    file_base = os.path.splitext(file_name)[0] # get file name without extension
-    save_dir = os.path.join(output_folder, file_base)
-    os.makedirs(save_dir, exist_ok=True)
-    return numeric_cols, save_dir
+from src.utils.hupa_ucm.hupa_ucm_loaders import get_numeric_columns, prepare_output
+
 
 # Create and save boxplots for numeric df columns to visualize outliers
 def plot_outliers(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabetes Dataset/EDA_Outputs/outliers"):
-    numeric_cols,save_dir = prepare_plotting(df, file_name, output_folder)
-    if numeric_cols is None:
+    numeric_cols = get_numeric_columns(df)
+    if len(numeric_cols) == 0:
+        print(f"No numeric columns in {file_name}")
         return
+
+    file_base, save_dir = prepare_output(file_name, output_folder, "outliers")
 
     for col in numeric_cols:
         plt.figure(figsize=(6, 4))
@@ -31,9 +26,12 @@ def plot_outliers(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabe
 # Create and save histogram for numeric df columns to visualize outliers
 
 def plot_distributions(df, file_name, output_folder="../../../Datasets/HUPA-UCM Diabetes Dataset/EDA_Outputs/distributions"):
-    numeric_cols, save_dir = prepare_plotting(df, file_name, output_folder)
-    if numeric_cols is None:
+    numeric_cols = get_numeric_columns(df)
+    if len(numeric_cols) == 0:
+        print(f"No numeric columns in {file_name}")
         return
+
+    file_base, save_dir = prepare_output(file_name, output_folder, "outliers")
 
     for col in numeric_cols:
         plt.figure(figsize=(6, 4))
